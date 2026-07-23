06b80a81b35fc197bde291b27f78a3bec9f18569
pedromtbarcelos <pedromtbarcelos@gmail.com>
2026-07-23 16:53:09 -0300

    Adicionando exceptions

---
+++ src/main/java/com/pedromiguel/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package com.pedromiguel.agendadortarefas.infrastructure.exceptions;
+
+import javax.naming.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+    public UnauthorizedException(String mensagem) {
+        super(mensagem);
+    }
+
+    public UnauthorizedException(String mensagem, Throwable throwable) {
+        super(mensagem);
+    }
+}
